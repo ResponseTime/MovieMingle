1d75eaed52edf144d42b59e1b09163afc7b14500
Looper <aayush.bhattacharjee2002@gmail.com>
2024-01-14 16:48:49 +0530

    fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
-console.log("hello")
+import express from 'express';
+
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.json({ success: true })
+})
+
+app.listen(3000, () => {
+    console.log('listening on port 3000')
+})
